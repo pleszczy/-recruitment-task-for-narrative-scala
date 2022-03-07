c7d4b78971a9663c67756d107460a12df7a769f7
Piotr <piotrkun@gmail.com>
2022-03-07 23:13:01 +0100

    Adding a druid query for analytics

---
+++ src/main/scala/analytics/druid/Druid.scala
@@ -0,0 +1,20 @@
+package org.narrative
+package analytics.druid
+
+import analytics.QueryParameters.Timestamp
+
+object Druid {
+
+  //  TODO: Druid is using a variation of HyperLogLog for COUNT(DISTINCT userID). Set useApproximateCountDistinct to "false"
+  def query(timestamp: Timestamp): Unit = {
+    s"""
+       |SELECT
+       |SUM("count") FILTER(WHERE eventType = 'click') AS clicks,
+       |SUM("count") FILTER(WHERE eventType = 'impression') AS impressions,
+       |COUNT(DISTINCT userId) AS unique_users
+       |FROM "analytics-v1"
+       |WHERE __time >= DATE_TRUNC('hour', MILLIS_TO_TIMESTAMP($timestamp)) - INTERVAL '1' HOUR
+       |""".stripMargin
+  }
+
+}
